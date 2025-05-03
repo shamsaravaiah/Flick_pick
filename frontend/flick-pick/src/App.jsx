3c651e089ab329b1ab47360d8512785ecafe13ea
Your Name <ghazalchamto@gmail.com>
2025-05-03 18:53:55 +0200

    created routes

--- frontend/flick-pick/src/App.jsx
+++ frontend/flick-pick/src/App.jsx
@@ -1,7 +1,40 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import React from "react";
+import LoginForm from "./pages/auth/loginForm";
+import SignUpForm from "./pages/Auth/SignUpForm";
+import Home from "./pages/Dashboard/Home";
 
 const App = () => {
   return (
-    <div className="">  App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signUp" element={<SignUpForm />} />
+          <Route path="/dashboard" element={<Home />} />
+          <Route path="/create-poll" element={<CreatePoll />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
+
+export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
