edf0f6f05c1e77433889745f13585f7b57657d3d
bereket tesfaye <berekettesfaye7909@gmail.com>
2025-05-08 22:12:01 +0200

    added a middleware for uploading image

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,27 @@
+// middleware/upload.js
+const multer = require("multer");
+const path = require("path");
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// File filter
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Unsupported file type"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
