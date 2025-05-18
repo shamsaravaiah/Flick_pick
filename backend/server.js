b95158bafcc5eae04192b395e4f9afb7d0924233
bereket tesfaye <berekettesfaye7909@gmail.com>
2025-05-18 23:05:39 +0200

    made the backend to server unkown routes from dist folder of the frontend

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,11 @@ app.use("/api/v1/poll", pollRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// This *must* come after your API routes:
 
+app.use(express.static(path.join(__dirname, 'frontend/flick-pick/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/flick-pick/dist', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
